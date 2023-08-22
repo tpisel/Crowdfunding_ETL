f485f46c80a4153caee6e12de7e8344715ea7659
Tom Pisel <mail@tompisel.com>
2023-08-22 21:44:12 +1000

    add database schema

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ The crowdfunding dataset has been extracted via the following notebooks:
 - Campaign data, by Helena in `ETL_Mini_Project_HChen.ipynb`
 - Contacts data, by Tom in `ETL_Mini_Project_TPisel.ipynb`
 
+The table schema is defined in `crowdfunding_db_schema.sql` and visualised in `db_diagram.png`
+
 No external code has been used in this project, except where provided as part of the assignment resources.
---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,51 @@
+﻿CREATE TABLE category
+(
+    category_id VARCHAR(16) NOT NULL,
+    category UNIQUE TEXT NOT NULL,
+    CONSTRAINT pk_category
+        PRIMARY KEY (category_id)
+);
+
+CREATE TABLE subcategory
+(
+    subcategory_id VARCHAR(16) NOT NULL,
+    subcategory UNIQUE TEXT NOT NULL,
+    CONSTRAINT pk_subcategory
+        PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE contacts
+(
+    contact_id BIGINT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    CONSTRAINT pk_contacts
+        PRIMARY KEY (contact_id)
+);
+
+CREATE TABLE campaign
+(
+    cf_id BIGINT NOT NULL,
+    contact_id BIGINT NOT NULL,
+    company_name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    goal DOUBLE PRECISION NOT NULL,
+    pledged DOUBLE PRECISION NOT NULL,
+    outcome TEXT NOT NULL,
+    backers_count BIGINT NOT NULL,
+    country VARCHAR(2) NOT NULL,
+    currency VARCHAR(3) NOT NULL,
+    launched_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(16) NOT NULL,
+    subcategory_id VARCHAR(16) NOT NULL,
+    CONSTRAINT pk_campaign
+        PRIMARY KEY (cf_id),
+    CONSTRAINT fk_campaign_contact_id
+        FOREIGN KEY (contact_id) REFERENCES contacts,
+    CONSTRAINT fk_campaign_category_id
+        FOREIGN KEY(category_id) REFERENCES category,
+    CONSTRAINT fk_campaign_subcategory_id
+        FOREIGN KEY(subcategory_id) REFERENCES subcategory
+);
